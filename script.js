3f2776b7eefaf936b9b372c3939713d0d27bf89b
Venkatesh A <venkia.venkatesh07@gmail.com>
2026-04-13 10:59:04 +0530

    Day 1 JS basics

---
+++ script.js
@@ -0,0 +1,24 @@
+const name = "Venkatesh";
+const age = 21;
+const city = "banglore";
+const Profession = "Student";
+const skills = "JavaScript";
+
+console.log(`My Name is ${name}, I am ${age}, from ${city}, i am a ${Profession},my skill is ${skills}`);
+
+const a = 15;
+const b = 5;
+
+console.log("Add:",a+b);
+console.log("sub:",a-b);
+console.log("mul:",a*b);
+console.log("Div:",a/b);
+
+
+console.log("Adult:",age>=18);
+
+console.log(x)
+
+
+
+
